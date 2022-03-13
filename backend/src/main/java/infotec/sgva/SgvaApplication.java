db4bb9761438060de35698bfb162c2a7297d962b
crisurubu <cristianourubu@gmail.com>
2022-03-13 16:00:43 -0300

    Projeto criado

---
+++ backend/src/main/java/infotec/sgva/SgvaApplication.java
@@ -0,0 +1,13 @@
+package infotec.sgva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgvaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/infotec/sgva/SgvaApplicationTests.java
@@ -0,0 +1,13 @@
+package infotec.sgva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
